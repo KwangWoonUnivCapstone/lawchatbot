51626c9fd8c8bb7f4ab023a540a339d95ced2738
arthur <iii1003@naver.com>
2024-05-04 20:42:50 +0900

    data: data 편집

--- chatbot/getdata.py
+++ chatbot/getdata.py
@@ -1,8 +1,8 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders import UnstructuredPDFLoader
-pdf_filepath = '/Users/eungwanhwi/Desktop/lawchatbotproject/venv/lawchatbot/criminal_law.pdf'
+pdf_filepath = 'C:/Users/user/OneDrive/바탕 화면/lawchatbot/criminal_law.pdf'
 #openssopenssl versionloader = UnstructuredPDFLoader(pdf_filepath)
 loader = PyPDFLoader(pdf_filepath)
 pages = loader.load()
 
-len(pages)
+print(len(pages))
