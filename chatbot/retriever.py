999c55f55f134308c369171d42aa263f17aa8d12
gwanhwi <rhksgnl3@naver.com>
2024-05-04 20:22:03 +0900

    fix

--- chatbot/retriever.py
+++ chatbot/retriever.py
@@ -1,14 +1,18 @@
 from langchain.document_loaders import DirectoryLoader
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Chroma
+from langchain_core.prompts import PromptTemplate
+from langchain.schema.runnable import RunnablePassthrough
 import os
 from glob import glob
 
-files = glob(os.path.join('./', '*.xml'))
-loader = DirectoryLoader('./data', glob="**/*.xml")
-
-형사판결_docs = loader.load()
-행정판결_docs = []  # Add this line to define the variable "행정판결_docs"
+files = glob(os.path.join('', '*.xml'))
+loader = DirectoryLoader('C:/Users/user/OneDrive/바탕 화면/lawchatbot/chatbot/data/2021', glob="**/*.xml")
+# loader2 = DirectoryLoader('C:/Users/user/OneDrive/바탕 화면/lawchatbot/chatbot/data/2021', glob="**/*.xml")
+# loader3 = DirectoryLoader('C:/Users/user/OneDrive/바탕 화면/lawchatbot/chatbot/data/2021', glob="**/*.xml")
+# loader4 = DirectoryLoader('C:/Users/user/OneDrive/바탕 화면/lawchatbot/chatbot/data/2021', glob="**/*.xml")
+data_docs = []
+data_docs = loader.load() # Add this line to define the variable "행정판결_docs"
 
 model_name = "jhgan/ko-sroberta-multitask" # (KorNLU 데이터셋에 학습시킨 한국어 임베딩 모델)
 model_kwargs = {'device': 'cpu'}
@@ -18,7 +22,11 @@ embedding_model = HuggingFaceEmbeddings(
     model_kwargs=model_kwargs,
     encode_kwargs=encode_kwargs
 )
-행정판결_docs[0]
+# print(data_docs[0])
 
-형사판결_vectorstore = Chroma.from_documents(documents=형사판결_docs, embedding=embedding_model)
+data_vectorstore = Chroma.from_documents(documents=data_docs, embedding=embedding_model)
 # 행정판결_vectorstore = Chroma.from_documents(documents=행정판결_docs, embedding=embedding_model)
+data_retriever = data_vectorstore.as_retriever(search_kwargs={"k": 1})
+retrieved_docs = data_retriever.invoke(
+    "절도 사건에 관한 판례를 알려줘"
+)
